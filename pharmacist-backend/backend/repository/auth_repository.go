cbdd3c4c3cecd04146a32330eabfdabd44c43f4f
danielababu <daniel0ababu@gmail.com>
2025-06-29 16:06:36 +0300

    User Authentication

--- pharmacist-backend/backend/repository/auth_repository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "pharmacist-backend/backend/domain"
-
-type AuthRepository interface {
-	FindByEmail(email string) (*domain.User, error)
-}
--- pharmacist-backend/backend/repository/medicine_repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "pharmacist-backend/backend/domain"
-
-type MedicineRepository interface {
-	Search(query string) ([]domain.Medicine, error)
-	FindByID(id uint) (*domain.Medicine, []domain.MedicineVariant, error)
-}
