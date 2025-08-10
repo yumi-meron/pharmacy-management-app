692a2014e3bd84367751f20671533aa7a2b806fb
Meron Weldu <meron.w.teka@gmail.com>
2025-08-10 18:52:07 +0100

    datalayer update

--- pharmacist_mobile/lib/domain/repositories/cart_repository.dart
+++ pharmacist_mobile/lib/domain/repositories/cart_repository.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:pharmacist_mobile/domain/entities/cart_data_entity.dart';
 import '../../../core/error/failure.dart';
 import '../entities/cart_item.dart';
 
 abstract class CartRepository {
-  Future<Either<Failure, List<CartItem>>> getCartItems();
+  Future<Either<Failure, CartData>> getCartItems();
   Future<Either<Failure, Unit>> removeCartItem(String id);
   Future<Either<Failure, Unit>> addCartItem(String medicineVariantId, int quantity);
   Future<Either<Failure, Unit>> checkoutCart();
